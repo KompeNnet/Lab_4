6e1cc8df76abc186286c8fc0611e397983a7ea22
Krompy <kompennet@gmail.com>
2017-05-14 19:22:37 +0300

    reloading fixed a little bit._.

--- Dlls/MainDll/MainDll/Helpers/Hierarchy.cs
+++ Dlls/MainDll/MainDll/Helpers/Hierarchy.cs
@@ -1,13 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 using Lab_4.Loaders;
 
 namespace Lab_4.Helpers
 {
-     public class Hierarchy
+    public class Hierarchy
      {
         public List<string> BookChild { get; set; }
         public BookLoader Loader { get; set; }
