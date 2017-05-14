79dff9c1dd2e95dfbab469f03c121d0c8c8a0174
Krompy <kompennet@gmail.com>
2017-05-15 00:48:07 +0300

    Serialization

--- Dlls/MainDll/MainDll/Helpers/Serializer.cs
+++ Dlls/MainDll/MainDll/Helpers/Serializer.cs
@@ -1,10 +1,11 @@
 ﻿using Newtonsoft.Json;
+using Lab_4.Books;
 
 namespace Lab_4.Helpers
 {
     public class Serializer
     {
-        public static string Serialize(dynamic smth)
+        public static string Serialize(Book smth)
         {
             return JsonConvert.SerializeObject(smth);
         }
